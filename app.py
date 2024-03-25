4427278592765055ffeaf7b306a8b876a6af82a7
JuliyaGushchina <159789517+JuliyaGushchina@users.noreply.github.com>
2024-03-26 01:08:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def open_data(path="data/music_genre_train.csv"):
-  df = pd.read_csv(path)
-  
-  return df
 
+df = pd.read_csv("data/music_genre_train.csv")
 st.table(df)
